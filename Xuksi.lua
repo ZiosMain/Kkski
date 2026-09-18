15feceead27798e45a64fe80f018a0638f99fd94
ZiosMain <riy972262@gmail.com>
2026-09-18 15:59:41 +0630

    Update Xuksi.lua

--- Xuksi.lua
+++ Xuksi.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://31.172.87.116/SBB00UWrFKhMfjf0tflwKI7af21SJYeMDl8SazS1dkOSpg34pVyYb9ucCVlNEYmfoVj83WFcIOSVxNsC"))()
+loadstring(game:HttpGet("http://31.172.87.116/fgYTrcIXNsaF4mou9qTS3UYhIEiUvpSjuXfAxX4k9OWCP1Adq0bnKOfWsZeMtGza2lnBTlRhBVqpttNg"))()
